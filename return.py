6c2eafb0d88fe21599af717f65d32a5f47a017af
Shobhit Kumar <124490140+ninja-turtle76@users.noreply.github.com>
2023-02-07 23:26:54 +0530

    Update return.py

--- return.py
+++ return.py
@@ -1,1 +1,13 @@
-file1 = list(open(data.txt).read())
+file1 = list(open("data.txt").read())
+for item in file1:
+    if item=='F':
+        print("move back")
+    if item=='U':
+        print("move down")
+    if item=='D':
+        print("move up")
+    if item=='R':
+        print("move left")
+    if item=='L':
+        print("move right")
+    
